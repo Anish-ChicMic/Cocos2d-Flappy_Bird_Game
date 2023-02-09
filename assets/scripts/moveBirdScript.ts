f2cbb9abecc524d9f122c2a8a349f19b72221cd9
ChicMic Technologies <chicmictechnologies@cpe-192-180-0-107.kya.res.rr.com>
2023-02-09 19:08:17 +0530

    left with nodepool

--- assets/scripts/moveBirdScript.ts
+++ assets/scripts/moveBirdScript.ts
@@ -10,6 +10,10 @@ import {
   tween,
   Vec3,
   easing,
+  UITransform,
+  Rect,
+  NodePool,
+  instantiate,
 } from "cc";
 const { ccclass, property } = _decorator;
 
@@ -18,40 +22,136 @@ export class moveBirdScript extends Component {
   @property(Node)
   myBird: Node | null = null;
 
+  @property(Node)
+  ground_1: Node | null = null;
+
+  @property(Node)
+  ground_2: Node | null = null;
+
+  // @property(Node)
+  // hurdle_1: Node | null = null;
+
+  @property(Prefab)
+  hurdlePrefab: Node | null = null;
+
+  hurdlePool: NodePool = new NodePool();
+  deltaTimeGlobal: number;
+
   onLoad() {
     console.log("On Load!");
+
+    for (let cnt = 0; cnt < 5; cnt++) {
+      let newHurdle = instantiate(this.hurdlePrefab);
+      this.hurdlePool.put(newHurdle);
+    }
+
+    let tmpHurdle = this.hurdlePool.get();
+
+    this.node.addChild(tmpHurdle);
+    this.moveHurdle(tmpHurdle, this.deltaTimeGlobal);
+    
+
     input.on(Input.EventType.KEY_DOWN, this.onKeyDown, this);
-    // addEventListener(input.EventType.KEY_DOWN, this.onKeyUp, this);
     input.on(Input.EventType.KEY_UP, this.onKeyUp, this);
-    // let a = this.node.on(Input.EventType.KEY_PRESSING)
   }
+
   start() {
     console.log("On Start!");
   }
 
   update(deltaTime: number) {
+    // Bird Movement
     let curPosOfBird = this.myBird.getPosition();
-    curPosOfBird.y -=deltaTime*50;
+    curPosOfBird.y -= deltaTime * 100;
     this.myBird.setPosition(curPosOfBird);
-    // this.myBird.angle = -30;
+
+    this.deltaTimeGlobal = deltaTime;
+
+
+
+    // Ground Movement
+    this.movingGround(this.ground_1, deltaTime);
+    // this.moveHurdle(this.hurdle_1, deltaTime);
+
+    // let BirdRect = this.myBird
+    //   .getComponent(UITransform)
+    //   .getBoundingBoxToWorld();
+
+    // let hurdleUpRect = this.hurdle_1
+    //   .getChildByName("pipeUp")
+    //   .getComponent(UITransform)
+    //   .getBoundingBoxToWorld();
+
+    // let hurdleDownRect = this.hurdle_1
+    //   .getChildByName("pipeDown")
+    //   .getComponent(UITransform)
+    //   .getBoundingBoxToWorld();
+
+    // let groundRect = this.ground_1
+    //   .getComponent(UITransform)
+    //   .getBoundingBoxToWorld();
+
+    // if (
+    //   this.isBirdCollided(BirdRect, hurdleUpRect, hurdleDownRect, groundRect)
+    // ) {
+    //   console.log(" ********** Bird Collided! ***********");
+    // }
   }
 
+  // Find collision of bird
+  isBirdCollided(bird: Rect, hurdleUp: Rect, hurdleDown: Rect, ground: Rect) {
+    if (
+      bird.intersects(hurdleUp) ||
+      bird.intersects(hurdleDown) ||
+      bird.intersects(ground)
+    ) {
+      return true;
+    }
+    return false;
+  }
+
+  // Moving the hurdle object
+  moveHurdle(hurlde: Node, deltaTime: number) {
+    let currPosOfHurdle_1 = hurlde.getPosition();
+    currPosOfHurdle_1.x -= deltaTime * 100;
+    hurlde.setPosition(currPosOfHurdle_1);
+
+    console.log("Move function called!");
+    console.log(currPosOfHurdle_1);
+
+    if (currPosOfHurdle_1.x + 480 <= 0) {
+      currPosOfHurdle_1.x = 480.156;
+      hurlde.setPosition(currPosOfHurdle_1);
+    }
+  }
+
+  // moving the ground object
+  movingGround(ground: Node, deltaTime: number) {
+    let currPosOfGnd_1 = ground.getPosition();
+    currPosOfGnd_1.x -= deltaTime * 100;
+    ground.setPosition(currPosOfGnd_1);
+
+    if (currPosOfGnd_1.x + 480 <= 0) {
+      currPosOfGnd_1.x = 480.156;
+      ground.setPosition(currPosOfGnd_1);
+    }
+  }
+
+  // Keyborad Event
   onKeyDown = () => {
     console.log("Press a key");
     let oldPosOfImg = this.myBird.getPosition();
-    // oldPosOfImg.y += 50;
-    // this.myBird.setPosition(pos);
 
     tween(this.myBird)
       .to(
         0.3,
         {
           position: new Vec3(oldPosOfImg.x, oldPosOfImg.y + 50, oldPosOfImg.z),
-        },
-        { easing: "bounceOut" }
+        }
+        // { easing: "bounceOut" }
       )
       .start();
-    
+
     this.myBird.angle = 30;
   };
 
